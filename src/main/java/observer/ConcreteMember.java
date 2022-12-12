1389d8ce162d8b30460c9442b4a106c326dd5f52
Avi Rahimov <rahimovavi3@gmail.com>
2022-12-13 00:42:58 +0200

    Implemented some functions

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -1,8 +1,9 @@
 package observer;
 
 public class ConcreteMember implements Member{
+    private UndoableStringBuilder stringBuilder;
     @Override
     public void update(UndoableStringBuilder usb) {
-
+        this.stringBuilder = usb;
     }
 }
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -1,14 +1,25 @@
 package observer;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class GroupAdmin implements Sender{
+    private ArrayList<UndoableStringBuilder> conditions;
+    private ArrayList<Member> members;
+    private Stack<UndoableStringBuilder> stk;
+    public GroupAdmin(){
+        this.members = new ArrayList<>();
+        this.conditions = new ArrayList<>();
+        this.stk = new Stack<>();
+    }
     @Override
     public void register(Member obj) {
-
+        this.members.add(obj);
     }
 
     @Override
     public void unregister(Member obj) {
-
+        this.members.remove(obj);
     }
 
     @Override
