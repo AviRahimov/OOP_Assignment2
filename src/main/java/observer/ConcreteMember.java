4fa8d0513bd2286bdc3a19677384130daa569ef7
liorvi35 <liorvi35@gmail.com>
2022-12-29 22:22:31 +0200

    Update ConcreteMember.java

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -4,17 +4,28 @@ package observer;
  * The ConcreteMember class is an observer class that implements from Member Interface that are responsible to
  * update the members due to changes in UndoableStringBuilder in GroupAdmin class(the observable class).
  */
-public class ConcreteMember implements Member{
+public class ConcreteMember implements Member {
+    /**
+     * USB object that CM will hold
+     */
     private UndoableStringBuilder stringBuilder;
+
+    /**
+     * name for each CM object
+     */
     private String member_name;
 
-    public ConcreteMember(String member_name){
+    /**
+     * standart constructore for CM objects
+     * @param member_name - name that will be assigned to any CM object
+     */
+    public ConcreteMember(String member_name) {
         this.member_name = member_name;
         this.stringBuilder = new UndoableStringBuilder();
     }
 
     /**
-     *
+     * this function is updates the USB object
      * @param usb describe the UndoableStringBuilder that need to be replaced by the current UndoableStringBuilder
      *            object that belong to the member.
      */
@@ -24,6 +35,10 @@ public class ConcreteMember implements Member{
         System.out.println("The member updated successfully");
     }
 
+    /**
+     * this function is standrart toString() method
+     * @return fields of current CM object
+     */
     @Override
     public String toString() {
         return "ConcreteMember{" + "name=" + member_name +
@@ -31,7 +46,11 @@ public class ConcreteMember implements Member{
                 '}';
     }
 
+    /**
+     * standart get method for CM's USB object
+     * @return the current CM's USB
+     */
     public UndoableStringBuilder getUSB() {
-        return stringBuilder;
+        return this.stringBuilder;
     }
 }
