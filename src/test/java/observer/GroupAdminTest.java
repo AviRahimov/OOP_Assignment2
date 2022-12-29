fc73b42bb431e63a2cfbedb53c262d7fd5ff8b63
liorvi35 <liorvi35@gmail.com>
2022-12-29 12:24:11 +0200

    Update GroupAdminTest.java

--- src/test/java/observer/GroupAdminTest.java
+++ src/test/java/observer/GroupAdminTest.java
@@ -23,10 +23,29 @@ class GroupAdminTest {
 
     @Test
     void unregister() {
-        GroupAdmin reg = new GroupAdmin();
-        ConcreteMember member2 = new ConcreteMember("member2");
-        reg.register(member2);
-        reg.unregister(member2);
+        // creating new CMs to register and unregister from GA
+        ConcreteMember mem1 = new ConcreteMember("MEMBER1");
+        ConcreteMember mem2 = new ConcreteMember("MEMBER2");
+        ConcreteMember mem3 = new ConcreteMember("MEMBER3");
+        ConcreteMember mem4 = new ConcreteMember("MEMBER4");
+
+        // creating new GA
+        GroupAdmin adm = new GroupAdmin();
+
+        // registering CM's
+        adm.register(mem1);
+        adm.register(mem2);
+        adm.register(mem3);
+        adm.register(mem4);
+
+        // unregistering CM's
+        adm.unregister(mem1);
+        adm.unregister(mem2);
+        adm.unregister(mem3);
+        adm.unregister(mem4);
+
+        // checking if list of members is empty
+        assertTrue(adm.getMembers().isEmpty());
     }
 
     @Test
@@ -67,10 +86,13 @@ class GroupAdminTest {
         adm.append(app);
 
         // checking append
-        String str;
+        String str = "";
         int i = 0;
         for(Member m : adm.getMembers()) {
-            str = m.toString();
+            // this condition is always True!
+            if(m instanceof ConcreteMember) {
+                str = ((ConcreteMember) m).getUSB().toString();
+            }
             assertEquals(old[i] + app, str);
             i++;
         }
@@ -107,8 +129,11 @@ class GroupAdminTest {
         adm.undo();
         adm.undo();
 
-        // checking if ok
-        assertEquals("6OsWPmMZpXLtgYrGtRRk", adm.getMembers().get(0).toString());
+        // checking if ok (this condition is always True!)
+        if(adm.getMembers().get(0) instanceof ConcreteMember) {
+            assertEquals("6OsWPmMZpXLtgYrGtRRk", ((ConcreteMember)adm.getMembers().get(0)).getUSB().toString());
+        }
+
 
     }
 }
